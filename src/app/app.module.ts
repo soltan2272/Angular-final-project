f6b77ea2078e553870b21b2baabe7235aadf41cf
unknown <alielsaeed1121@gmail.com>
2021-12-19 02:42:05 +0200

    upload img in cloudinaryس

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -26,6 +26,7 @@ import { PaymentComponent } from './Components/payment/payment.component';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { TranslateModule,TranslateLoader } from '@ngx-translate/core';
 import { PaypalComponent } from './Components/paypal/paypal.component';
+import {NgxDropzoneModule } from 'ngx-dropzone';
 
 export function createTranslateLoader(http:HttpClient)
 {
@@ -65,6 +66,7 @@ export function createTranslateLoader(http:HttpClient)
     NgxPaginationModule,
     LazyLoadImageModule,
     UserModuleModule,
+    NgxDropzoneModule,
     TranslateModule.forRoot({
       loader: {
           provide: TranslateLoader,
